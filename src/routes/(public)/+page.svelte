dda8d8bb7faed1e4d94bde77bd242622fd5afe14
Shindra David <shindradavid.dev@gmail.com>
2023-03-14 20:16:57 +0300

    Add google fonts for headings

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -4,7 +4,7 @@
   name = name.toUpperCase();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>design studio</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="scss">
