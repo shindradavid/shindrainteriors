98277a34c968c654daf5676339155b8f1dd1f2a1
Shindra David <shindradavid.dev@gmail.com>
2023-03-14 21:02:17 +0300

    Add montserrat font

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -4,7 +4,7 @@
   name = name.toUpperCase();
 </script>
 
-<h1>design studio</h1>
+<h1>Shindra design studio</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="scss">
