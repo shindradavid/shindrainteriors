730c2b791367d454b657d1035f529a06e36922a7
Shindra David <shindradavid.dev@gmail.com>
2023-03-14 19:12:15 +0300

    Add initial theme

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+  let name = 'Jon Doe';
+
+  name = name.toUpperCase();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+</style>
