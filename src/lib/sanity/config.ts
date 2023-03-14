172a7eb4c69628ca25c8dcb61d3391828f4b810e
Shindra David <shindradavid.dev@gmail.com>
2023-03-14 12:19:40 +0300

    Format code

--- src/lib/sanity/config.ts
+++ src/lib/sanity/config.ts
@@ -4,13 +4,13 @@ import { deskTool } from 'sanity/desk';
 // import {schemaTypes} from './schemas'
 
 export default defineConfig({
-	name: 'default',
-	title: 'shindradesignstudio',
-	basePath: '/studio',
-	projectId: '10s406h0',
-	dataset: 'production',
-	plugins: [deskTool() /*visionTool()*/],
-	schema: {
-		// types: schemaTypes,
-	}
+  name: 'default',
+  title: 'shindradesignstudio',
+  basePath: '/studio',
+  projectId: '10s406h0',
+  dataset: 'production',
+  plugins: [deskTool() /*visionTool()*/],
+  schema: {
+    // types: schemaTypes,
+  }
 });
--- src/routes/(studio)/studio/[...catchall]/+page.svelte
+++ src/routes/(studio)/studio/[...catchall]/+page.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-	import { renderStudio, defineConfig } from 'sanity';
-	import { onMount } from 'svelte';
+  import { renderStudio, defineConfig } from 'sanity';
+  import { onMount } from 'svelte';
 
-	import config from '$lib/sanity/config';
+  import config from '$lib/sanity/config';
 
-	let studioEl: HTMLDivElement;
+  let studioEl: HTMLDivElement;
 
-	onMount(() => {
-		if (studioEl) {
-			const sanityConfig = defineConfig(config);
+  onMount(() => {
+    if (studioEl) {
+      const sanityConfig = defineConfig(config);
 
-			renderStudio(studioEl, sanityConfig);
-		}
-	});
+      renderStudio(studioEl, sanityConfig);
+    }
+  });
 </script>
 
 <svelte:head>
-	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-	<meta name="robots" content="noindex" />
-	<meta name="referrer" content="same-origin" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+  <meta name="robots" content="noindex" />
+  <meta name="referrer" content="same-origin" />
 </svelte:head>
 
 <main>
-	<div bind:this={studioEl} />
+  <div bind:this={studioEl} />
 </main>
 
 <style lang="scss">
-	main {
-		height: 100vh;
-		max-height: 100vh;
-		overscroll-behavior: none;
-		-webkit-font-smoothing: antialiased;
-		overflow: auto;
+  main {
+    height: 100vh;
+    max-height: 100vh;
+    overscroll-behavior: none;
+    -webkit-font-smoothing: antialiased;
+    overflow: auto;
 
-		div {
-			height: 100%;
-		}
-	}
+    div {
+      height: 100%;
+    }
+  }
 </style>
