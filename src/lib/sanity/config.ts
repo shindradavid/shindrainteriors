a6f519467069c834a4ec36277102a9efe4081fa9
Shindra David <shindradavid.dev@gmail.com>
2023-03-16 12:37:11 +0300

    Run prettier

--- src/lib/sanity/config.ts
+++ src/lib/sanity/config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
-import {visionTool} from '@sanity/vision';
+import { visionTool } from '@sanity/vision';
 // import {schemaTypes} from './schemas'
 import { colorInput } from '@sanity/color-input';
 
